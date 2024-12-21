b114a276b7d42df0ef25fcd31d046ba54064a228
dantaKK <xckznbxck@qq.com>
2024-12-21 14:38:29 +0800

    vue-react

---
+++ vue_react/cli/try-vue1/src/App.vue
@@ -0,0 +1,11 @@
+
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <Counter />
+  </div>
+</template>
+
+<script setup>
+import Counter from './components/Counter.vue'
+</script>
---
+++ vue_react/cli/try-vue1/src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="counter">
+        <p>Count:{{ count }}</p>
+        <button @click="Increment">+1</button>
+        2
+    </div>
+</template>
+
+<script setup>
+  import {ref} from 'vue'
+
+  const count = ref(0)
+  const Increment = () => {
+    count.value++
+  }
+</script>
+<style >
+.counter{
+  text-align: center;
+  margin-top: 50px;
+}
+button{
+  padding: 10px 20px;
+  font-size: 16px;
+}
+</style>
