08e3a42a9f03df3ad06cacbcb1f03cd9fc5f4dda
dantaKK <xckznbxck@qq.com>
2025-01-03 21:52:49 +0800

    router

---
+++ vue_react/vue-router-demo/src/views/post/index/post-Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+       PostIndex
+    </div>
+</template>
+
+<script>
+    
+</script>
+
+<style  scoped>
+
+</style>
---
+++ vue_react/vue-router-demo/src/views/post/show/PostShow.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div>PostShow {{postId}}</div>
+    </div>
+</template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+const route = useRoute() // 当前路由
+
+const {postId} = route.params
+</script>
+
+<style  scoped>
+
+</style>
