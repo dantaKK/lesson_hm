4a518764dd8da2f0fc514646953bba7e1c8c1a09
dantaKK <xckznbxck@qq.com>
2025-01-04 17:03:28 +0800

    router

--- vue_react/vue-router-demo/src/router/index.js
+++ vue_react/vue-router-demo/src/router/index.js
@@ -10,24 +10,65 @@ import About from '../views/About.vue'
 // posts 模块 post文件夹
 import PostIndex from '../views/post/index/post-Index.vue'
 import PostShow from '../views/post/show/PostShow.vue'
+import PostMeta from '../views/post/show/components/post-meta.vue'
+// 文章模块
+const postRoutes=[
+    {
+        path:'/posts',
+        name:'postIndex',
+        component:PostIndex,
+        // 路由向页面级别组件传参  
+        props:{
+          sort:'popular',
+        },
+        meta:{
+        
+            title:"文章列表"
+        }
+    },
+    {
+        path:'/posts/:postId',
+        component:PostShow,
+        name:'postShow',
+        meta:{
+          
+            title:"文章详情"
+        },
+        props:true,
+        // 子路由， 路由的嵌套
+        children:[
+            {
+                path:'meta',
+                component:PostMeta,
+            }
+        ]
+
+    },
+    {
+
+    }
+]
+
 // 路由配置
 const routes=[
    {
     path:'/', // 路径
-    component:Home // 页面组件
+    component:Home, // 页面组件
+    meta:{
+        requireAuth:true,
+       title:"首页"
+    },
    },
    {
     path:'/about',
+    meta:{
+        requireAuth:true,
+        title:"关于"
+    },
+    name:'about',  // 路由的名称
     component:About
    },
-   {
-    path:'/posts',
-    component:PostIndex
-   },
-   {
-    path:'/posts/:postId', // 动态路由
-    component:PostShow 
-   }
+   ...postRoutes
 ]
 // 创建路由实例
 const router=createRouter({
@@ -35,6 +76,23 @@ const router=createRouter({
     history:createWebHashHistory(),
     // 路由模式
     routes  // 路由配置数组
+})
+
+// 路由守卫
+// 每次路由的切换都会执行这个回调
+router.beforeEach((to,from,next)=>{
+    document.title=to.meta.title || '智能办公'
+  // to 要去的路由
+  // from 离开的路由对象
+  // router +routes 构成路由
+//   console.log(to,from,'-----')
+if(to.meta.requireAuth){
+//   console.log('需要登录')
+
+// url,redirect 跳转
+  next('/login')
+}
+ 
 })
 
 export default router
--- vue_react/vue-router-demo/src/views/post/show/PostShow.vue
+++ vue_react/vue-router-demo/src/views/post/show/PostShow.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <div>PostShow {{postId}}</div>
+    
+        <router-view />
+
     </div>
 </template>
 
