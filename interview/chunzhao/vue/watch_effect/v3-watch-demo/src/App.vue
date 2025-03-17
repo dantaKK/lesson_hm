090a3abd08b23af21086e0b5995818a59c61ff4e
dantaKK <xckznbxck@qq.com>
2025-03-17 21:26:57 +0800

    watch_effect

---
+++ interview/chunzhao/vue/watch_effect/v3-watch-demo/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+   {{count}}
+  {{double}}
+</div>
+</template>
+
+<script setup>
+import { ref ,watchEffect} from 'vue'
+const count = ref(0);
+const double = ref(0);
+watchEffect(()=>{   
+  console.log(count.value);
+  console.log(double.value); 
+})
+setTimeout(()=>{
+  count.value++;
+  double.value = count.value * 2;
+},1000)
+
+</script>
+
+<style  scoped>
+
+</style>
---
+++ interview/chunzhao/vue/watch_effect/v3-watch-demo/src/App2.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+    {{count}}
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref ,watch} from 'vue'
+  const count = ref(0);
+  // 参数1 依赖的响应式数据 参数2 回调函数 
+  watch(count,(newValue,oldValue)=>{
+    console.log(newValue,oldValue);
+  },{
+    immediate:true, // 立即执行
+    deep:true   // 深度监听 { { } }
+  })
+  
+  setTimeout(()=>{
+    count.value++;
+  },1000)
+  </script>
+  
+  <style  scoped>
+  
+  </style>
